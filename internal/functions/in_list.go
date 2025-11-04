5cb498d46f094be7cf95ac3e17b0eb9e4edd0437
bobbyonmagic <bobbyonmagic@bobbyiliev.com>
2025-11-04 08:56:36 +0000

    Refine in_list function helpers

--- internal/functions/in_list.go
+++ internal/functions/in_list.go
@@ -58,77 +58,42 @@ func (inListFunction) Definition(_ context.Context, _ function.DefinitionRequest
 }
 
 func (inListFunction) Run(ctx context.Context, req function.RunRequest, resp *function.RunResponse) {
-	var value types.String
-	var allowed types.List
-	var ignoreCase types.Bool
-
-	if err := req.Arguments.GetArgument(ctx, 0, &value); err != nil {
-		resp.Error = err
+	value, state, ok := stringArgument(ctx, req, resp, 0)
+	if !ok {
 		return
 	}
 
-	if err := req.Arguments.GetArgument(ctx, 1, &allowed); err != nil {
-		resp.Error = err
-		return
-	}
-
-	if err := req.Arguments.GetArgument(ctx, 2, &ignoreCase); err != nil {
-		resp.Error = err
-		return
-	}
-
-	if value.IsNull() || value.IsUnknown() {
+	if state == valueUnknown {
 		resp.Result = function.NewResultData(types.BoolUnknown())
 		return
 	}
 
-	if allowed.IsUnknown() {
+	allowedValues, state, ok := allowedList(ctx, req, resp, 1)
+	if !ok {
+		return
+	}
+
+	if state == valueUnknown {
 		resp.Result = function.NewResultData(types.BoolUnknown())
 		return
 	}
 
-	if allowed.IsNull() {
-		resp.Error = function.NewFuncError("allowed values list must be provided")
+	ignore, state, ok := ignoreCaseFlag(ctx, req, resp, 2)
+	if !ok {
 		return
 	}
 
-	if ignoreCase.IsUnknown() {
+	if state == valueUnknown {
 		resp.Result = function.NewResultData(types.BoolUnknown())
 		return
 	}
 
-	var ignore bool
-	if ignoreCase.IsNull() {
-		ignore = false
-	} else {
-		ignore = ignoreCase.ValueBool()
-	}
-
-	var allowedValues []basetypes.StringValue
-	if diags := allowed.ElementsAs(ctx, &allowedValues, false); diags.HasError() {
-		diags.AddAttributeError(
-			path.Root("allowed"),
-			"Invalid Allowed Values",
-			"Allowed values must be provided as a list of strings.",
-		)
-		resp.Error = function.FuncErrorFromDiags(ctx, diags)
-		return
-	}
-
-	candidates := make([]string, 0, len(allowedValues))
-	for _, item := range allowedValues {
-		if item.IsNull() || item.IsUnknown() {
-			continue
-		}
-		candidates = append(candidates, item.ValueString())
-	}
-
-	if len(candidates) == 0 {
+	if len(allowedValues) == 0 {
 		resp.Result = function.NewResultData(types.BoolUnknown())
 		return
 	}
 
-	validator := validators.NewInListValidator(candidates, ignore)
+	validator := validators.NewInListValidator(allowedValues, ignore)
 
 	validation := frameworkvalidator.StringResponse{}
 	validator.ValidateString(ctx, frameworkvalidator.StringRequest{
@@ -142,4 +107,95 @@ func (inListFunction) Run(ctx context.Context, req function.RunRequest, resp *fu
 	}
 
 	resp.Result = function.NewResultData(basetypes.NewBoolValue(true))
+}
+
+func stringArgument(ctx context.Context, req function.RunRequest, resp *function.RunResponse, index int) (types.String, valueState, bool) {
+	var value types.String
+	if err := req.Arguments.GetArgument(ctx, index, &value); err != nil {
+		resp.Error = function.NewFuncError(err.Error())
+		return types.String{}, valueKnown, false
+	}
+
+	if value.IsNull() || value.IsUnknown() {
+		return value, valueUnknown, true
+	}
+
+	return value, valueKnown, true
+}
+
+func allowedList(ctx context.Context, req function.RunRequest, resp *function.RunResponse, index int) ([]string, valueState, bool) {
+	var allowed types.List
+	if err := req.Arguments.GetArgument(ctx, index, &allowed); err != nil {
+		resp.Error = function.NewFuncError(err.Error())
+		return nil, valueKnown, false
+	}
+
+	values, state, err := prepareAllowedValues(ctx, allowed)
+	if err != nil {
+		resp.Error = function.NewFuncError(err.Error())
+		return nil, valueKnown, false
+	}
+
+	return values, state, true
+}
+
+func ignoreCaseFlag(ctx context.Context, req function.RunRequest, resp *function.RunResponse, index int) (bool, valueState, bool) {
+	var flag types.Bool
+	if err := req.Arguments.GetArgument(ctx, index, &flag); err != nil {
+		resp.Error = function.NewFuncError(err.Error())
+		return false, valueKnown, false
+	}
+
+	value, state := boolFromOptional(flag)
+	return value, state, true
+}
+
+type valueState int
+
+const (
+	valueKnown valueState = iota
+	valueUnknown
+)
+
+func boolFromOptional(v types.Bool) (bool, valueState) {
+	if v.IsUnknown() {
+		return false, valueUnknown
+	}
+
+	if v.IsNull() {
+		return false, valueKnown
+	}
+
+	return v.ValueBool(), valueKnown
+}
+
+func prepareAllowedValues(ctx context.Context, list types.List) ([]string, valueState, error) {
+	if list.IsUnknown() {
+		return nil, valueUnknown, nil
+	}
+
+	if list.IsNull() {
+		return nil, valueKnown, function.NewFuncError("allowed values list must be provided")
+	}
+
+	var items []basetypes.StringValue
+	diags := list.ElementsAs(ctx, &items, false)
+	if diags.HasError() {
+		diags.AddAttributeError(
+			path.Root("allowed"),
+			"Invalid Allowed Values",
+			"Allowed values must be provided as a list of strings.",
+		)
+		return nil, valueKnown, function.FuncErrorFromDiags(ctx, diags)
+	}
+
+	values := make([]string, 0, len(items))
+	for _, item := range items {
+		if item.IsNull() || item.IsUnknown() {
+			continue
+		}
+		values = append(values, item.ValueString())
+	}
+
+	return values, valueKnown, nil
 }
