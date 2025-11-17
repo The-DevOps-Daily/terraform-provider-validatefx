03f665ead5fdf9f3623f18b2ccd974eaa679eaab
bobbyonmagic <bobbyonmagic@bobbyiliev.com>
2025-11-17 09:16:07 +0000

    feat(public_ip): add public_ip validator and function with tests, examples, docs and integration (closes #259)

---
+++ examples/functions/public_ip/function.tf
@@ -0,0 +1,16 @@
+locals {
+  addrs = [
+    "8.8.8.8",
+    "1.1.1.1",
+  ]
+}
+
+output "public_ip_example" {
+  value = [
+    for a in local.addrs : {
+      ip    = a
+      valid = provider::validatefx::public_ip(a)
+    }
+  ]
+}
+
